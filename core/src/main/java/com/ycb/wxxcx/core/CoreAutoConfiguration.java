3fea970ba8bddb281f25231ae6742d89b11716cf
zhuhui <pzzhuhui@gmail.com>
2017-06-15 18:07:06 +0800

    Initial commit

---
+++ core/src/main/java/com/ycb/wxxcx/core/CoreAutoConfiguration.java
@@ -0,0 +1,12 @@
+package com.ycb.wxxcx.core;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * Created by zhuhui on 17-6-15.
+ */
+@Configuration
+@EnableWebMvc
+public class CoreAutoConfiguration {
+}
---
+++ core/src/main/java/com/ycb/wxxcx/core/DefaultRibbonConfiguration.java
@@ -0,0 +1,8 @@
+package com.ycb.wxxcx.core;
+
+/**
+ * Created by zhuhui on 17-6-15.
+ */
+public class DefaultRibbonConfiguration {
+
+}
